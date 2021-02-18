b0ca1858d405481eb12347e6464b19a5480db4b3
amitkrmaharana <ak657harley@gmail.com>
2021-02-18 19:10:35 +0530

    Added: added first name pattern

--- UserRegistration.java
+++ UserRegistration.java
@@ -1,24 +1,16 @@
 import java.util.Scanner;
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class UserRegistration {
-	public static void firstNameCheck(String name) { // to check if first name input matches with the provided pattern 
-		String regex = "^[A-Z]{1}[a-zA-Z]{2,}$"; // 1st letter is Capital and has minimum of 3 letters
-		Pattern pattern = Pattern.compile(regex);
-		Matcher match = pattern.matcher(name);
-		boolean bool = match.matches();// checks if the pattern matches with the given pattern and returns a boolean value 
-		if (bool) 
-			System.out.println(name + " is a valid name");
-		else
-			System.out.println(name + " is not a valid name");
+	static Scanner sc = new Scanner(System.in);
+	public static void validation(String firstName) {
+		// First Name comes with restriction such as first letter should be upper case and with minimum 3 characters
+		System.out.println("First Name: " + Pattern.matches("^[A-Z]{1}[a-zA-Z]{2,}$", firstName));
 	}
 	public static void main(String[] args) {
 		System.out.println("Welcome to User Registration Program");
-		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter a valid first name");
-		String firstName = sc.next();
-		firstNameCheck(firstName);
+		String firstName = sc.nextLine();
+		validation(firstName);
 	}
-
 }
