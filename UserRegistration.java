d7a5a0cf10b545864cb5ee45beb089e830a7ef14
amitkrmaharana <ak657harley@gmail.com>
2021-02-18 08:44:44 +0530

    Added: added mobile number pattern

--- UserRegistration.java
+++ UserRegistration.java
@@ -33,18 +33,31 @@ public class UserRegistration {
 		else
 			System.out.println(email + " is not a valid email");
 	}
+	public static void mobilenumberCheck(String mobile) {
+		String regex = "^[9][1][\s][6-9][0-9]{9}$"; // 91 followed by space followed by 10 digit number   
+		Pattern pattern = Pattern.compile(regex);
+		Matcher match = pattern.matcher(mobile);
+		boolean bool = match.matches(); 
+		if (bool) 
+			System.out.println(mobile + " is a valid mobile number");
+		else
+			System.out.println(mobile + " is not a valid mobile number");
+	}
 	public static void main(String[] args) {
 		System.out.println("Welcome to User Registration Program");
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter a valid first name");
-		String firstName = sc.next();
+		String firstName = sc.nextLine();
 		System.out.println("Enter a valid last name");
-		String lastName = sc.next();
+		String lastName = sc.nextLine();
 		System.out.println("Enter a valid email");
-		String email = sc.next();
+		String email = sc.nextLine();
+		System.out.println("Enter a valid mobile number starting with 91 folowwed by a space and 10 didit number");
+		String mobile = sc.nextLine();
 		firstNameCheck(firstName);
 		lastNameCheck(lastName);
 		emailCheck(email);
+		mobilenumberCheck(mobile);
 	}
 
 }
